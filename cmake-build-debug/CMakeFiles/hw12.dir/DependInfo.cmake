824694e2188d1d2a8ad1ba51808638c37ceb24eb
yifanxing <xing.yif@Husky.neu.edu>
2017-04-13 15:03:50 -0400

    added ssh key yifan

---
+++ cmake-build-debug/CMakeFiles/hw12.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/yifanxing/Documents/学习/NU Years-2017/Computing Systems/hw12/hints/pages.c" "/Users/yifanxing/Documents/学习/NU Years-2017/Computing Systems/hw12/cmake-build-debug/CMakeFiles/hw12.dir/hints/pages.c.o"
+  "/Users/yifanxing/Documents/学习/NU Years-2017/Computing Systems/hw12/hints/slist.c" "/Users/yifanxing/Documents/学习/NU Years-2017/Computing Systems/hw12/cmake-build-debug/CMakeFiles/hw12.dir/hints/slist.c.o"
+  "/Users/yifanxing/Documents/学习/NU Years-2017/Computing Systems/hw12/nufs.c" "/Users/yifanxing/Documents/学习/NU Years-2017/Computing Systems/hw12/cmake-build-debug/CMakeFiles/hw12.dir/nufs.c.o"
+  "/Users/yifanxing/Documents/学习/NU Years-2017/Computing Systems/hw12/storage.c" "/Users/yifanxing/Documents/学习/NU Years-2017/Computing Systems/hw12/cmake-build-debug/CMakeFiles/hw12.dir/storage.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
