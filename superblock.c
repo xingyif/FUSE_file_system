315e57567dc0c25fad9a351b13dc324a0bd02aa3
yifanxing <xing.yif@Husky.neu.edu>
2017-04-18 21:50:25 -0400

    optimized import

--- superblock.c
+++ superblock.c
@@ -1,18 +1,7 @@
 
 #include <string.h>
 
-#include <sys/mman.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-#include <assert.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "superblock.h"
-#include "storage.h"
 //import blocks
 //import pages
 //import inode
