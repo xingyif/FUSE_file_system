9fbb3549e098b836505903d13ca24448a3ee24ce
brekru212 <krutiansky.b@husky.neu.edu>
2017-04-19 16:50:33 -0400

    not getting bus error anymore

--- pages.h
+++ pages.h
@@ -2,19 +2,19 @@
 #define PAGES_H
 
 #include <stdio.h>
-
+/*
 typedef struct inode {
     int refs; // reference count
     int mode; // permission & type
     int size; // bytes for file
     int xtra; // more stuff can go here
 } pnode;
-
+*/
 void   pages_init(const char* path);
 void   pages_free();
-void*  pages_get_page(int pnum);
+/*void*  pages_get_page(int pnum);
 inode* pages_get_node(int node_id);
 int    pages_find_empty();
 void   print_node(pnode* node);
-
+*/
 #endif
